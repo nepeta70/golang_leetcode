3456b1fd3b7a6b959606ebada16e556d481c825e
Eva Raimundez <nepeta@gmail.com>
2025-11-22 16:38:47 +0000

    twoSum and rotateImage

--- algorithms/easy_array.go
+++ algorithms/easy_array.go
@@ -75,13 +75,65 @@ func moveZeroes(nums []int) {
 }
 
 func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
 	return []int{}
 }
 
 func isValidSudoku(board [][]byte) bool {
+
 	return false
 }
 
 func rotateImage(matrix [][]int) {
+	d := len(matrix)
+	y0 := 0
+	d0 := d - 1
+	d1 := d0
+	d2 := d1
 
+	for i := y0; i < d1; i++ {
+		for j := 0; j < d2; j++ {
+			row0 := i
+			col0 := i + j
+			previousCell := matrix[row0][col0]
+
+			// new column index = initial row index
+			// new row index = d0 - initial column index
+			col1 := d0 - row0
+			nextCell := matrix[col0][col1]
+
+			// Top to right
+			matrix[col0][col1] = previousCell
+
+			// new row index = previous column index
+			// new column index = d0 - previous row index
+			col2 := d0 - col0
+			previousCell = matrix[col1][col2]
+
+			// Right to bottom
+			matrix[col1][col2] = nextCell
+
+			// new column index = previous row index
+			// new row index = d0 - previous column index
+			col3 := d0 - col1
+			nextCell = matrix[col2][col3]
+
+			// Bottom to left
+			matrix[col2][col3] = previousCell
+
+			// new row index = previous column index = initial row index
+			// new column index = d0 - previous row index = initial column index
+			// Left to top
+			matrix[row0][col0] = nextCell
+		}
+		y0++
+		d1--
+		d2 -= 2
+	}
 }
